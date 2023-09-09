fd8fa0dc16e418a09fb7d5345984564979887f46
Pratik Fuse <pratikfuse20@gmail.com>
2023-09-09 14:23:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 function fibonacci(num) {
 // your code here
+	  let next = 1;
+  let curr = 0;
+  let temp = 0;
+  let ans = 0;
+  for (let i = 0; i < num; i++) {
+    ans = curr;
+    curr = next + curr;
+    temp = next;
+    next = curr;
+    curr = temp;
+  }
+
+  return ans;
+	
 }
 
 module.exports = fibonacci;
